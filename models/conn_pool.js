17b990f030b2bfac5c97651f31110834ad64512a
应承峻 <1052257712@qq.com>
2019-06-17 20:40:38 +0800

    merge G1 & G2 & G3

--- models/conn_pool.js
+++ models/conn_pool.js
@@ -5,7 +5,7 @@ module.exports = (function() {
 		host: 'localhost',
 		user: 'root',
 		password: '',
-		database: 'opmysystem',
+		database: 'opmsystem',
 		port: '3306',
 		charset: 'utf8mb4'
 	});
